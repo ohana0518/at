65f009c28a1222e9402d071bc1d24121e2823401
ohana0518 <cxz411125@gmail.com>
2021-01-06 15:33:03 +0800

    new file:   t25.py

---
+++ t25.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import socket
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+import time
+import http.client as http
+import urllib
+import json
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(4, GPIO.IN)
+GPIO.setup(17, GPIO.IN)
+
+deviceId = "DyKg0wqU"
+deviceKey = "6mv09evG6uibcDKB" 
+
+def post_to_mcs(payload): 
+        headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
+        not_connected = 1 
+        while (not_connected):
+                try:
+                        conn = http.HTTPConnection("api.mediatek.com:80")
+                        conn.connect() 
+                        not_connected = 0 
+                except (http.HTTPException, socket.error) as ex: 
+                        print ("Error: %s" % ex)
+                        time.sleep(1)
+			 # sleep 10 seconds 
+        conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
+        response = conn.getresponse() 
+        print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
+        data = response.read() 
+        conn.close() 
+try:
+    while True:
+        h0, t0= Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 4)
+        SwitchStatus = GPIO.input(17)
+        if( SwitchStatus == 0):
+            print('Button pressed')
+        else:
+            print('Button released')
+        print('Temp={0:0.1f}*  '.format(t0))
+        payload = {"datapoints":[{"dataChnId":"Temperature","values":{"value":t0}},{"dataChnId":"button","values":{"value":SwitchStatus}}]}	
+        post_to_mcs(payload)
+        time.sleep(1)
+except KeyboardInterrupt:
+    print('關閉程式')
